7d13c3e28df24ecf0b874ec35c7f54489a1448d7
TracyAntonia <aantonjuguna@gmail.com>
2023-07-30 08:39:09 +0300

    completed assignments

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, {useState}from "react";
 
 function Item({ name, category }) {
+  const [inCart, setListClass] = useState(false);
+  function handleAddToCart() {
+    setListClass(!inCart);
+  }
+
   return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
-  );
+    <li className={inCart ? "in-cart" : ""}>
+    <span>{name}</span>
+    <span className="category">{category}</span>
+    <button className= {inCart ? "remove" : "add"} onClick={handleAddToCart}> 
+      {inCart ? "Remove from Cart" : "Add to Cart"}
+    </button>
+  </li>
+);
 }
 
 export default Item;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,24 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  function handleSelectCategory(e){
+    setSelectedCategory(e.target.value) 
+  }
+  const updatedList = items.filter(list => {
+    if(selectedCategory ==="All"){
+      return true
+    }else{
+    return list.category === selectedCategory
+    }
+  }) 
+
   return (
     <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div>
-      <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
+    <div className="Filter">
+      <select name="filter" onChange={handleSelectCategory}>
+        <option value="All">{selectedCategory}</option>
+        <option value="Produce">Produce</option>
+        <option value="Dairy">Dairy</option>
+        <option value="Dessert">Dessert</option>
+      </select>
     </div>
-  );
+    <ul className="Items">
+      {updatedList.map((item) => (
+        <Item key={item.id} name={item.name} category={item.category} />
+      ))}
+    </ul>
+  </div>
+);
 }
 
 export default ShoppingList;
